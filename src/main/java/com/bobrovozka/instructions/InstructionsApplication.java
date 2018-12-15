b53db8f921958ceaa7a8af6d22177360fe6a8f1c
@kurnakov92 <kurnakov92@gmail.com>
2018-12-16 00:33:06 +0700

    Initial commit

---
+++ src/main/java/com/bobrovozka/instructions/InstructionsApplication.java
@@ -0,0 +1,14 @@
+package com.bobrovozka.instructions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstructionsApplication.class, args);
+    }
+
+}
+
