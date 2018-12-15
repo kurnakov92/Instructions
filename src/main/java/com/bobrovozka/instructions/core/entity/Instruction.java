77632441f6aceaf73a4d9a7cfaa17c6c3e8c0f7d
@kurnakov92 <kurnakov92@gmail.com>
2018-12-16 02:00:49 +0700

    add created & last updated fields

--- src/main/java/com/bobrovozka/instructions/core/entity/Instruction.java
+++ src/main/java/com/bobrovozka/instructions/core/entity/Instruction.java
@@ -10,9 +10,17 @@ public class Instruction {
 
     @Id
     private String id;
+    private long created;
+    private long lastUpdated;
     private String name;
     private String description;
     private String body;
     private AccessType accessType;
 
+    public Instruction(String name, String description, String body, AccessType accessType) {
+        this.name = name;
+        this.description = description;
+        this.body = body;
+        this.accessType = accessType;
+    }
 }
